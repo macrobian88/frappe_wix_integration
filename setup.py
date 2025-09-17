be648624dd1fe2bf85e453bbf19caa0195d027b1
macrobian88 <ramshankarsc9@gmail.com>
2025-09-17 10:21:05 +0530

    Fix setup.py for proper nested structure

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in wix_integration/__init__.py
-from wix_integration import __version__ as version
+# get version from __version__ variable in wix_integration/wix_integration/__init__.py
+from wix_integration.wix_integration import __version__ as version
 
 setup(
     name="wix_integration",
